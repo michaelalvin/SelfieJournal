92dc95376083a84dcfaca046c58cd55d53b4929c
Michael Alvin <michael.alvin@stonybrook.edu>
2018-01-19 16:23:04 -0500

    Commit #2

--- imagepicker/ChartsViewController.swift
+++ imagepicker/ChartsViewController.swift
@@ -12,11 +12,31 @@ import Charts
 class ChartsViewController: UIViewController {
 
     
-    var numbers : [Double] = [] //This is where we are going to store all the numbers. This can be a set of numbers that come from a Realm database, Core data, External API's or where ever else
+    var numbers = [12.0, 22.0, 12.0, 32.0] //This is where we are going to store all the numbers. This can be a set of numbers that come from a Realm database, Core data, External API's or where ever else
+    
+    let months = ["Happy", "Sad", "Angry", "Surprised"]
+    let unitsSold = [20.0, 4.0, 6.0, 3.0]
     
     @IBOutlet weak var lineChart: LineChartView!
     
-    func updateGraph(){
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    @IBOutlet weak var emotionSegment: UISegmentedControl!
+   
+    @IBAction func emotionSegmentTapped(_ sender: Any) {
+        if(emotionSegment.selectedSegmentIndex == 0) {
+            lineChart.isHidden = true
+            pieChart.isHidden = false
+            setChart(dataPoints: months, values: unitsSold)
+        } else {
+            lineChart.isHidden = false
+            pieChart.isHidden = true
+            updateLineGraph()
+        }
+    }
+    
+    
+    func updateLineGraph(){
         var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
         
         //here is the for loop
@@ -33,15 +53,39 @@ class ChartsViewController: UIViewController {
         data.addDataSet(line1) //Adds the line to the dataSet
         data.setDrawValues(true)
         
-        line1.colors = [UIColor.magenta] //Sets the colour to magenta
-        line1.setCircleColor(UIColor.magenta)
-        line1.circleHoleColor = UIColor.magenta
+        var gradientColors = [UIColor.magenta.cgColor, UIColor.clear.cgColor] as CFArray
+        var color = UIColor.magenta
+        
+        if(emotionSegment.selectedSegmentIndex == 1) {
+            line1.colors = [UIColor.magenta] //Sets the colour to magenta
+            line1.setCircleColor(UIColor.magenta)
+            line1.circleHoleColor = UIColor.magenta
+        } else if (emotionSegment.selectedSegmentIndex == 2){
+            color = UIColor(rgb: 0x559efc)
+            line1.colors = [color] //Sets the colour to magenta
+            line1.setCircleColor(color)
+            line1.circleHoleColor = color
+            gradientColors = [color.cgColor, UIColor.clear.cgColor] as CFArray
+        } else if (emotionSegment.selectedSegmentIndex == 3){
+            color = UIColor(rgb: 0xff2d2d)
+            line1.colors = [color] //Sets the colour to magenta
+            line1.setCircleColor(color)
+            line1.circleHoleColor = color
+            gradientColors = [color.cgColor, UIColor.clear.cgColor] as CFArray
+        } else {
+            color = UIColor(rgb: 0x64dd49)
+            line1.colors = [color] //Sets the colour to magenta
+            line1.setCircleColor(color)
+            line1.circleHoleColor = color
+            gradientColors = [color.cgColor, UIColor.clear.cgColor] as CFArray
+        }
+        
+        
         line1.circleRadius = 4.0
         
         lineChart.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInSine)
         
         //Gradient fill
-        let gradientColors = [UIColor.magenta.cgColor, UIColor.clear.cgColor] as CFArray
         let colorLocations: [CGFloat] = [1.0, 0.0]
         guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else { print("gradient error"); return;
         }
@@ -60,17 +104,71 @@ class ChartsViewController: UIViewController {
         lineChart.chartDescription?.text = "January 1 - 7"
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        numbers.append(12)
-        numbers.append(22)
-        numbers.append(12)
-        numbers.append(32)
+    func setChart(dataPoints: [String], values: [Double]) {
         
-        updateGraph()
-        // Do any additional setup after loading the view.
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry1 = PieChartDataEntry(value: values[i], label: dataPoints[i])
+            dataEntries.append(dataEntry1)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "")
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        
+        var colors: [UIColor] = []
+        
+        //Happy
+        var color = UIColor.magenta
+        colors.append(color)
+        
+        //Sad
+        color = UIColor(rgb: 0x559efc)
+        colors.append(color)
+        
+        //Angry
+        color = UIColor(rgb: 0xff2d2d)
+        colors.append(color)
+        
+        //Surprised
+        color = UIColor(rgb: 0x64dd49)
+        colors.append(color)
+        
+        //Pie Chart Modifications
+        pieChart.usePercentValuesEnabled = true
+        pieChart.drawSlicesUnderHoleEnabled = false
+        pieChart.holeRadiusPercent = 0.60
+        pieChart.transparentCircleRadiusPercent = 0.43
+        pieChart.drawHoleEnabled = true
+        pieChart.rotationAngle = 0.0
+        pieChart.rotationEnabled = true
+        pieChart.highlightPerTapEnabled = false
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        formatter.maximumFractionDigits = 1
+        formatter.multiplier = 1.0
+        pieChartData.setValueFormatter(DefaultValueFormatter(formatter:formatter))
+        
+        //Pie Chart Setup
+        pieChartDataSet.colors = colors
+        pieChart.legend.enabled = true
+        pieChart.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInSine)
+        pieChart.data = pieChartData
+        pieChart.chartDescription?.text = "January 1 - 7"
     }
+    
+    override func viewDidLoad() {
+        emotionSegment.tintColor = UIColor(rgb: 0xffbb4f)
+        
+        super.viewDidLoad()
+        
+        lineChart.isHidden = true
+        pieChart.isHidden = false
+        setChart(dataPoints: months, values: unitsSold)
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
